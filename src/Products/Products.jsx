1fb21e04b03c984028899fc5f637b63b53f26355
Lokendra Nath <lokendraverma225@gmail.com>
2025-02-20 11:45:23 +0530

    update the props validation

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,9 +1,14 @@
+import PropTypes from "prop-types";
+
 const Products = ({ result }) => {
   return (
     <section className="absolute top-[8rem] flex-wrap ml-[19rem] mt-[2rem] z-[-2] grid grid-cols-4">
       {result}
     </section>
   );
+};
+Products.propTypes = {
+  result: PropTypes.node.isRequired,
 };
 
 export default Products;
